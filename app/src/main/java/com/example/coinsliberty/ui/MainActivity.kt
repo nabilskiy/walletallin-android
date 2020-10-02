ffb2e07774512bd467d3099d11c038bc41fe0381
AlexFil21 <alexey.filatov21@gmail.com>
2020-10-02 18:03:29 +0300

    start

--- app/src/main/java/com/example/coinsliberty/ui/MainActivity.kt
+++ app/src/main/java/com/example/coinsliberty/ui/MainActivity.kt
@@ -1,12 +1,39 @@
 package com.example.coinsliberty.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.coinsliberty.R
+import com.example.coinsliberty.utils.extensions.setTransparentLightStatusBar
+import com.example.coinsliberty.utils.extensions.setupFullScreen
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setTransparentLightStatusBar()
+        setupFullScreen()
+
+        lavCoinsLiberty.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+                Log.e("Animation:", "start")
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                Toast.makeText(this@MainActivity, "Finish Animation", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+                Log.e("Animation:", "cancel")
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+                Log.e("Animation:", "repeat")
+            }
+        })
     }
 }
