73587486fb3e3bfc2d2b45f6018ba6e5375b2d82
iNapalm <apoloninoleg@gmail.com>
2020-10-12 19:28:43 +0300

    forgot_pass

---
+++ app/src/main/java/com/example/coinsliberty/dialogs/ForgotPassDialog.kt
@@ -0,0 +1,29 @@
+package com.example.coinsliberty.dialogs
+
+
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+import org.koin.android.viewmodel.ext.android.viewModel
+import com.example.coinsliberty.R
+import com.example.coinsliberty.base.BaseKotlinDialogFragment
+import com.example.coinsliberty.utils.stub.StubViewModel
+
+class ForgotPassDialog : BaseKotlinDialogFragment() {
+    override val layoutRes: Int = R.layout.dialog_forgot_pass
+    override val viewModel: StubViewModel by viewModel()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        isCancelable = true
+    }
+
+    companion object {
+        val TAG: String = ForgotPassDialog::class.java.name
+        fun newInstance(): DialogFragment {
+            val fragment = ForgotPassDialog()
+            fragment.setStyle(STYLE_NO_FRAME, 0)
+            return fragment
+        }
+    }
+}
--- app/src/main/java/com/example/coinsliberty/ui/demo/DemoActivity.kt
+++ app/src/main/java/com/example/coinsliberty/ui/demo/DemoActivity.kt
@@ -3,10 +3,7 @@ package com.example.coinsliberty.ui.demo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.coinsliberty.R
-import com.example.coinsliberty.dialogs.AcceptDialog
-import com.example.coinsliberty.dialogs.ErrorDialog
-import com.example.coinsliberty.dialogs.QrCodeDialog
-import com.example.coinsliberty.dialogs.SendDialog
+import com.example.coinsliberty.dialogs.*
 import kotlinx.android.synthetic.main.activity_demo.*
 
 
@@ -17,15 +14,8 @@ class DemoActivity : AppCompatActivity() {
         setContentView(R.layout.activity_demo)
 
         btnTest.setOnClickListener {
-            SendDialog.newInstance(
-                "SEnD ETH",
-                " 16azaQHPThoQcf14o2XqmbG",
-                "0.25947531",
-                "2671.53",
-                "ETC",
-                "USD"
-            )
-                .show(supportFragmentManager, SendDialog.TAG)
+            ForgotPassDialog.newInstance()
+                .show(supportFragmentManager, ForgotPassDialog.TAG)
         }
 //        btnDialogError.setOnClickListener {
 //            ErrorDialog.newInstance("this tittle")
