8e936fb77b9f50ae14711ab88dc68c36ef539d6f
iNapalm <apoloninoleg@gmail.com>
2020-10-12 11:52:55 +0300

    Merge branch 'master' of C:\Users\iNapalm\AndroidProject\coinsliberty-androidv3 with conflicts.

--- app/src/main/java/com/example/coinsliberty/ui/dialogs/ChangeLanguageViewModel.kt
+++ app/src/main/java/com/example/coinsliberty/ui/dialogs/ChangeLanguageViewModel.kt
@@ -1,31 +1,45 @@
 package com.example.coinsliberty.ui.dialogs
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.example.coinsliberty.R
 import com.example.coinsliberty.base.BaseViewModel
 import com.example.coinsliberty.model.LanguageContent
-import com.example.coinsliberty.model.SharedPreferencesProvider
+import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.item_change_language.view.*
+import kotlinx.android.synthetic.main.toolbar.view.*
 
-class ChangeLanguageViewModel(private val shared: SharedPreferencesProvider) : BaseViewModel() {
+class ChangeLanguageViewModel() : BaseViewModel() {
 
     val languagesLiveData: MutableLiveData<List<LanguageContent>> = MutableLiveData(getListData())
-    //val currentLanguageLiveData: MutableLiveData<String> = MutableLiveData(shared.getLanguage().toString())
+    val currentLanguagesLiveData: MutableLiveData<LanguageContent> = MutableLiveData()
+
 
     fun getListData(): ArrayList<LanguageContent> {
         val listData: ArrayList<LanguageContent> = ArrayList()
 
-//        listData.add(LanguageContent(R.string.english, R.drawable.ic_unitedstates, false))
-//        listData.add(LanguageContent(R.string.russian, R.drawable.ic_russia, false))
-//        listData.add(LanguageContent(R.string.arabic, R.drawable.ic_united_arab_emirates, false))
-//        listData.add(LanguageContent(R.string.deutch, R.drawable.ic_germany, false))
-//        listData.add(LanguageContent(R.string.italiano, R.drawable.ic_italy, false))
-//        listData.add(LanguageContent(R.string.portuguese, R.drawable.ic_brazil, false))
-//        listData.add(LanguageContent(R.string.espanol, R.drawable.ic_spain, false))
+        listData.add(LanguageContent(R.string.english, R.drawable.ic_unitedstates, false))
+        listData.add(LanguageContent(R.string.russian, R.drawable.ic_russia, false))
+        listData.add(LanguageContent(R.string.arabic, R.drawable.ic_united_arab_emirates, false))
+        listData.add(LanguageContent(R.string.deutch, R.drawable.ic_germany, false))
+        listData.add(LanguageContent(R.string.italiano, R.drawable.ic_italy, false))
+        listData.add(LanguageContent(R.string.portuguese, R.drawable.ic_brazil, false))
+        listData.add(LanguageContent(R.string.espanol, R.drawable.ic_spain, false))
 
         return listData
     }
 
-//    fun setLanguage(languageContent: LanguageContent) {
-//        shared.setLanguage(languageContent.name.toString())
-//    }
+    fun changeList(list: LanguageContent) {
+        val listData: ArrayList<LanguageContent> = getListData()
+        for (i in listData) {
+            if (i.name.equals(list.name)) {
+                Log.d("eee", currentLanguagesLiveData.toString())
+                i.active = true
+                currentLanguagesLiveData.postValue(i)
+            }
+        }
+        languagesLiveData.postValue(listData)
+    }
+
+
 }
