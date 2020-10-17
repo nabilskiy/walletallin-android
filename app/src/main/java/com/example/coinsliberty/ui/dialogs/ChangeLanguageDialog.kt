f3d8ca1d51f8f968936ea2a985401004aacb7def
Maksim <maksim.boyko.96@gmail.com>
2020-10-17 10:21:25 +0300

    refactoring

--- app/src/main/java/com/example/coinsliberty/ui/dialogs/ChangeLanguageDialog.kt
+++ app/src/main/java/com/example/coinsliberty/ui/dialogs/ChangeLanguageDialog.kt
@@ -1,23 +1,17 @@
 package com.example.coinsliberty.ui.dialogs
 
 import android.annotation.SuppressLint
-import android.app.Activity
-import android.app.AlertDialog
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import com.example.coinsliberty.R
 import com.example.coinsliberty.base.BaseAdapter
 import com.example.coinsliberty.base.BaseKotlinDialogFragment
-import com.example.coinsliberty.di.viewModelModule
 import com.example.coinsliberty.model.LanguageContent
 import com.example.coinsliberty.ui.dialogs.adapter.ChangeLanguageHolder
 import com.example.coinsliberty.utils.extensions.bindDataTo
 import kotlinx.android.synthetic.main.dialog_change_language.*
 import kotlinx.android.synthetic.main.item_change_language.*
-import org.koin.android.experimental.dsl.viewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 
 
