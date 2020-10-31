06c97f7ac788d2ed4c2597150d912c15fcf4940c
iNapalm <apoloninoleg@gmail.com>
2020-10-31 21:38:51 +0200

    fix

--- app/src/main/java/com/example/coinsliberty/dialogs/ForgotPassViewModel.kt
+++ app/src/main/java/com/example/coinsliberty/dialogs/ForgotPassViewModel.kt
@@ -5,6 +5,8 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import com.example.coinsliberty.base.BaseViewModel
 import com.example.coinsliberty.data.ForgotPassRequest
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class ForgotPassViewModel(
     app: Application,
@@ -16,11 +18,12 @@ class ForgotPassViewModel(
 
     fun forgotPass(email: String) {
         launch(::onErrorHandler) {
+            withContext(Dispatchers.Main){onStartProgress.value = Unit}
             val response = repository.forgotPass(ForgotPassRequest(email))
             Log.e("!!!", response.toString())
-
             resultRecovery.postValue(response.result)
             messageRecovery.postValue(response.error?.message)
+            withContext(Dispatchers.Main){onEndProgress.value = Unit}
         }
     }
 
--- app/src/main/java/com/example/coinsliberty/ui/signup/SignUpViewModel.kt
+++ app/src/main/java/com/example/coinsliberty/ui/signup/SignUpViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.MutableLiveData
 import com.example.coinsliberty.base.BaseViewModel
 import com.example.coinsliberty.data.SignUpRequest
 import com.example.coinsliberty.ui.config.NavigationConfig
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class SignUpViewModel(
     app: Application,
@@ -14,8 +16,10 @@ class SignUpViewModel(
 
     fun signUp(email: String, password: String, firstName: String, lastName: String) {
         launch(::onErrorHandler) {
+            withContext(Dispatchers.Main){onStartProgress.value = Unit}
             val response = repository.signUp(SignUpRequest(email, password, firstName, lastName))
             Log.e("!!!", response.toString())
+            withContext(Dispatchers.Main){onEndProgress.value = Unit}
         }
     }
 
