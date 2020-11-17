e71d2da40c0d077f6cb0c1708d3f1c4311fa9ce0
AlexFil21 <alexey.filatov21@gmail.com>
2020-11-17 22:07:42 +0200

    exchange soon

--- app/src/main/java/com/coinsliberty/wallet/BottomFragmant.kt
+++ app/src/main/java/com/coinsliberty/wallet/BottomFragmant.kt
@@ -2,10 +2,13 @@ package com.coinsliberty.wallet
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.view.get
 import androidx.navigation.Navigation
-import com.coinsliberty.wallet.base.BaseKotlinFragment
-import com.coinsliberty.wallet.utils.stub.StubViewModel
 import com.coinsliberty.moneybee.utils.stub.StubNavigator
+import com.coinsliberty.wallet.base.BaseKotlinFragment
+import com.coinsliberty.wallet.dialogs.exchangeComingSoon.ExchangeDialog
+import com.coinsliberty.wallet.utils.stub.StubViewModel
 import kotlinx.android.synthetic.main.activity_bottom.*
 import org.koin.android.ext.android.get
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -17,15 +20,21 @@ class BottomFragmant : BaseKotlinFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var dialog = ExchangeDialog.newInstance()
+
         bottomNavigation.setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.wallet -> {
                     Navigation.findNavController(requireActivity(), R.id.frameLayout).navigate(R.id.action_global_my_wallet)
                     true
                 }
-                R.id.exchange -> {
-                    Navigation.findNavController(requireActivity(), R.id.frameLayout).navigate(R.id.action_global_exchange)
-                    true
+                 R.id.exchange -> {
+                     Navigation.findNavController(requireActivity(), R.id.frameLayout).navigate(R.id.action_global_exchange)
+                     dialog.show(childFragmentManager, ExchangeDialog.TAG)
+                     dialog.initListeners {
+                         if(it) bottomNavigation.selectedItemId = R.id.wallet
+                     }
+                     true
                 }
                 R.id.setting -> {
                     Navigation.findNavController(requireActivity(), R.id.frameLayout).navigate(R.id.action_global_settings)
