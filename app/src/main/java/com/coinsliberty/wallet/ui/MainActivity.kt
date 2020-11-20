f19ac57dab9e8d595d7c51bded1d01eebcaec1e9
iNapalm <apoloninoleg@gmail.com>
2020-11-21 00:37:35 +0200

    fix pin

--- app/src/main/java/com/coinsliberty/wallet/ui/MainActivity.kt
+++ app/src/main/java/com/coinsliberty/wallet/ui/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.coinsliberty.wallet.ui
+import android.app.PendingIntent.getActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.coinsliberty.wallet.R
 import com.coinsliberty.wallet.utils.extensions.setTransparentLightStatusBar
@@ -11,6 +13,7 @@ import com.coinsliberty.wallet.utils.extensions.setupFullScreen
 class MainActivity : AppCompatActivity() {
 
     private var isActivePin: Boolean? = null
+    //var currentFragment: Fragment = getActivity().getFragmentManager().findFragmentById(R.id.navHostFragment)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,23 +26,33 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-
         if (isActivePin == true) {
             Log.e("!!!", "isActive")
             Navigation.findNavController(this, R.id.navHostFragment).navigate(R.id.action_pin_code)
         }
     }
 
+
     override fun onPause() {
         super.onPause()
         Log.e("!!!", "onPause")
-        isActivePin = true
+        Log.e("!!!a", Navigation.findNavController(this, R.id.navHostFragment).currentDestination.toString())
+        val currentFragment: String = Navigation.findNavController(this, R.id.navHostFragment).currentDestination.toString()
+        val loginFragment: String = "Destination(com.coinsliberty.wallet:id/loginFragment) label=LoginFragment class=com.coinsliberty.wallet.ui.login.LoginFragment"
+        if(currentFragment != loginFragment) {
+            isActivePin = true
+        }
+
     }
 
     override fun onStop() {
         super.onStop()
         Log.e("!!!", "onStop")
-        isActivePin = true
+        val currentFragment: String = Navigation.findNavController(this, R.id.navHostFragment).currentDestination.toString()
+        val loginFragment: String = "Destination(com.coinsliberty.wallet:id/loginFragment) label=LoginFragment class=com.coinsliberty.wallet.ui.login.LoginFragment"
+        if(currentFragment != loginFragment) {
+            isActivePin = true
+        }
 
     }
 }
